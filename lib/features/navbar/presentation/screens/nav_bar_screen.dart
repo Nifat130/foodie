d2d30698ad2f9a3302924adf5ef1fab86b3ef40c
Nifat130 <mahingreen130@gmail.com>
2025-11-08 21:30:09 +0600

    navbar problem solved by Nifat

--- lib/features/navbar/presentation/screens/nav_bar_screen.dart
+++ lib/features/navbar/presentation/screens/nav_bar_screen.dart
@@ -15,87 +15,88 @@ class NavBarScreen extends StatelessWidget {
     return LayoutBuilder(
       builder: (context, constrains) {
         final width = constrains.maxWidth;
-        return Scaffold(
-          backgroundColor: AppColors.textWhite,
-          extendBody: true,
-          body: SafeArea(
-            child: Obx(
+        return SafeArea(
+          top: false,
+          child: Scaffold(
+            backgroundColor: AppColors.textWhite,
+            extendBody: true,
+            body: Obx(
               () => controller.listOfScreens[controller.currentIndex.value],
             ),
-          ),
-          bottomNavigationBar: width > 600 ?
-          SizedBox() : SafeArea(
-            child: Stack(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: AppColors.textWhite,
-                    boxShadow: [
-                      BoxShadow(
-                        color: AppColors.blueGrey.withAlpha(
-                          100,
-                        ), // shadow color
-                        blurRadius: 4,
-                        offset: const Offset(4, 4), // position of the shadow
-                      ),
-                    ],
+            bottomNavigationBar: width > 600 ?
+            SizedBox() : SafeArea(
+              child: Stack(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: AppColors.textWhite,
+                      boxShadow: [
+                        BoxShadow(
+                          color: AppColors.blueGrey.withAlpha(
+                            100,
+                          ), // shadow color
+                          blurRadius: 4,
+                          offset: const Offset(4, 4), // position of the shadow
+                        ),
+                      ],
+                    ),
+                    margin: EdgeInsets.only(top: 30),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        NavBarButton(
+                          controller: controller,
+                          buttonNumber: 0,
+                          icon: Icons.home_filled,
+                        ),
+                        NavBarButton(
+                          controller: controller,
+                          buttonNumber: 1,
+                          icon: Icons.favorite_border_rounded,
+                        ),
+                        NavBarButton(
+                          controller: controller,
+                          buttonNumber: 2,
+                          icon: Icons.shopping_cart_rounded,
+                        ),
+                        NavBarButton(
+                          controller: controller,
+                          buttonNumber: 3,
+                          icon: Icons.edit_note_outlined,
+                        ),
+                        NavBarButton(
+                          controller: controller,
+                          buttonNumber: 4,
+                          icon: Icons.menu,
+                        ),
+                      ],
+                    ),
                   ),
-                  margin: EdgeInsets.only(top: 30),
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      NavBarButton(
-                        controller: controller,
-                        buttonNumber: 0,
-                        icon: Icons.home_filled,
-                      ),
-                      NavBarButton(
-                        controller: controller,
-                        buttonNumber: 1,
-                        icon: Icons.favorite_border_rounded,
-                      ),
-                      NavBarButton(
-                        controller: controller,
-                        buttonNumber: 2,
-                        icon: Icons.shopping_cart_rounded,
-                      ),
-                      NavBarButton(
-                        controller: controller,
-                        buttonNumber: 3,
-                        icon: Icons.edit_note_outlined,
-                      ),
-                      NavBarButton(
-                        controller: controller,
-                        buttonNumber: 4,
-                        icon: Icons.menu,
-                      ),
-                    ],
-                  ),
-                ),
-                Positioned(
-                  top: 0,
-                  right: 0,
-                  left: 0,
-                  child: GestureDetector(
-                    onTap: (){
-                      controller.currentIndex.value = 2;
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: AppColors.success,
-                      ),
-                      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                      child: Icon(
-                        Icons.shopping_cart_rounded,
-                        color: AppColors.textWhite,
-                        size: 28,
+                  Positioned(
+                    top: 0,
+                    right: 0,
+                    left: 0,
+                    child: GestureDetector(
+                      onTap: (){
+                        controller.currentIndex.value = 2;
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: AppColors.success,
+                        ),
+                        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                        child: Icon(
+                          Icons.shopping_cart_rounded,
+                          color: AppColors.textWhite,
+                          size: 28,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         );
